7121c07df800e33402f90e60b9c18feed3fd9738
Wes Payne <noblepayne@noblepayne.com>
2023-10-26 19:31:06 -0700

    add demo2.py

---
+++ demo2.py
@@ -0,0 +1,39 @@
+import logging
+import time
+import uuid
+
+import rholog as ρ
+
+
+def step1():
+    time.sleep(1)
+
+
+def step2_substep1():
+    time.sleep(1)
+
+
+def step2_substep2(log):
+    ρ.log(log, "long sleep time")
+    time.sleep(4)
+    ρ.log(log, "done sleeping")
+    1 / 0
+
+
+def step2(log):
+    step2_substep1()
+    time.sleep(1)
+    step2_substep2(log)
+
+
+def main(log, root_id):
+    with ρ.trace(log, "main", root_id=root_id) as log:
+        step1()
+        step2(log)
+
+
+if __name__ == "__main__":
+    root_id = uuid.uuid4().hex
+    ρ.setup_logging()
+    log = logging.getLogger("main")
+    main(log, root_id)
